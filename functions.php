6b330927881ab0516dbf1fef98e65140abfaa52c
thekirankumardash <thekirankumardash@gmail.com>
2016-03-30 21:27:32 +0530

    0201 The Main Shop Template

--- functions.php
+++ functions.php
@@ -6,7 +6,10 @@ if ( !defined( 'ABSPATH' ) ) exit;
 //remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
 
 
+add_filter( 'loop_shop_columns', function(){ return 2; } );
 
+
+remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5);
 
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
